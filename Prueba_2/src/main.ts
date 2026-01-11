6c6d84935184328ddad94d647252003ca38aa49c
Andrea Fuente <andrea.fuente.fdez.a11y@gmail.com>
2026-01-11 19:48:35 +0100

    Prueba 2

--- Prueba_2/src/main.ts
+++ Prueba_2/src/main.ts
@@ -1,3 +1,84 @@
-import "./style.css";
+const tarjetaContainer = document.getElementById("container");
+const crearBoton = (): HTMLButtonElement | HTMLDivElement => {
+  const boton = document.createElement("button");
+  boton.type = "button";
+  boton.id = "boton-tarjeta";
 
-console.log("Hello Typescript!");
+  if (
+    tarjetaContainer !== null &&
+    tarjetaContainer !== undefined &&
+    tarjetaContainer instanceof HTMLElement
+  ) {
+    tarjetaContainer.appendChild(boton);
+    return boton;
+  } else {
+    const mensajeError = "No se ha encontrado el contenedor de la tarjeta";
+    const errorContainer = document.createElement("div");
+    errorContainer.id = "error-container";
+    errorContainer.textContent = mensajeError;
+    document.body.appendChild(errorContainer);
+    return errorContainer;
+  }
+};
+
+const nuevoBoton = crearBoton();
+
+//Comprobar si existe la imagen antes de pintarla
+const comprobarImagen = (boton: HTMLButtonElement): boolean => {
+  const imagenExistente = boton.querySelector("#imagen-tarjeta");
+  if (imagenExistente !== null) {
+    return true;
+  }
+  return false;
+};
+
+//Limpiar imagen existente
+const limpiarImagenExistente = (boton: HTMLButtonElement): void => {
+  const imagenExistente = boton.querySelector("#imagen-tarjeta");
+  if (imagenExistente !== null) {
+    imagenExistente.remove();
+  }
+};
+
+const pintarImagen = (boton: HTMLButtonElement): void => {
+  if (!comprobarImagen(boton)) {
+    const imagenTarjeta = document.createElement("img");
+    imagenTarjeta.id = "imagen-tarjeta";
+    imagenTarjeta.src =
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/refs/heads/main/memo/1.png";
+    imagenTarjeta.alt = "";
+    boton.appendChild(imagenTarjeta);
+  }
+};
+
+type estadoTarjeta = "CARA" | "DORSO";
+
+let estadoActual: estadoTarjeta = "DORSO";
+
+const pintarTarjeta = (boton: HTMLButtonElement, estado: estadoTarjeta): void => {
+        if (estado === "CARA") {
+      pintarImagen(boton);
+      boton.style.backgroundColor = "";
+    } else if (estado === "DORSO") {
+      //Borrar la imagen si existe
+      if (comprobarImagen(boton)) {
+        limpiarImagenExistente(boton);
+      }
+      boton.style.backgroundColor = "#aee2ff";
+    }
+  
+  };
+
+const handleJugar = (): void => {
+  if (nuevoBoton instanceof HTMLButtonElement) {
+    nuevoBoton.addEventListener("click", () => {
+      estadoActual = estadoActual === "DORSO" ? "CARA" : "DORSO";
+      pintarTarjeta(nuevoBoton,estadoActual);
+    });
+  }
+};
+if (nuevoBoton instanceof HTMLButtonElement) {
+  pintarTarjeta(nuevoBoton,"DORSO");
+}
+
+handleJugar();
